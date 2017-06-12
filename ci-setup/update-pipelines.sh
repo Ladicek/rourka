cec3a63910433af955604d145e69c0bb58b5b7cb
Ladislav Thon <lthon@redhat.com>
2017-06-12 16:51:46 +0200

    add Losiot PROD variant

--- ci-setup/update-pipelines.sh
+++ ci-setup/update-pipelines.sh
@@ -26,7 +26,7 @@ oc process ci-basic-template CI_NAME=ci-spring-boot-configmap-booster    CI_DESC
 oc process ci-sso-template   CI_NAME=ci-spring-boot-http-secured-booster CI_DESCRIPTION="Spring Boot | 103: SSO"          CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-http-secured-booster $SHARED_SECRET | oc apply -f -
 oc process ci-basic-template CI_NAME=ci-spring-boot-health-check-booster CI_DESCRIPTION="Spring Boot | 104: Health Check" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-health-check-booster $SHARED_SECRET | oc apply -f -
 
-# Losiot
+# Losiot: STAGE
 
 oc process losiot-template CI_NAME=losiot-stage-zip-wfswarm-rest-http    CI_DESCRIPTION="WildFly Swarm | 100: HTTP API"     CI_TYPE="launch-stage.openshift.io .zip" LOSIOT_TARGET=STAGE LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M100_HTTP_API     LOSIOT_RUNTIME=WILDFLY_SWARM | oc apply -f -
 oc process losiot-template CI_NAME=losiot-stage-zip-wfswarm-health-check CI_DESCRIPTION="WildFly Swarm | 104: Health Check" CI_TYPE="launch-stage.openshift.io .zip" LOSIOT_TARGET=STAGE LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M104_HEALTH_CHECK LOSIOT_RUNTIME=WILDFLY_SWARM | oc apply -f -
@@ -36,3 +36,15 @@ oc process losiot-template CI_NAME=losiot-stage-zip-vertx-health-checks-booster 
 
 oc process losiot-template CI_NAME=losiot-stage-zip-spring-boot-http-booster         CI_DESCRIPTION="Spring Boot | 100: HTTP API"     CI_TYPE="launch-stage.openshift.io .zip" LOSIOT_TARGET=STAGE LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M100_HTTP_API     LOSIOT_RUNTIME=SPRING_BOOT | oc apply -f -
 oc process losiot-template CI_NAME=losiot-stage-zip-spring-boot-health-check-booster CI_DESCRIPTION="Spring Boot | 104: Health Check" CI_TYPE="launch-stage.openshift.io .zip" LOSIOT_TARGET=STAGE LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M104_HEALTH_CHECK LOSIOT_RUNTIME=SPRING_BOOT | oc apply -f -
+
+# Losiot: PROD
+
+oc process losiot-template CI_NAME=losiot-prod-zip-wfswarm-rest-http    CI_DESCRIPTION="WildFly Swarm | 100: HTTP API"     CI_TYPE="launch.openshift.io .zip" LOSIOT_TARGET=PROD LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M100_HTTP_API     LOSIOT_RUNTIME=WILDFLY_SWARM | oc apply -f -
+oc process losiot-template CI_NAME=losiot-prod-zip-wfswarm-health-check CI_DESCRIPTION="WildFly Swarm | 104: Health Check" CI_TYPE="launch.openshift.io .zip" LOSIOT_TARGET=PROD LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M104_HEALTH_CHECK LOSIOT_RUNTIME=WILDFLY_SWARM | oc apply -f -
+
+oc process losiot-template CI_NAME=losiot-prod-zip-vertx-http-booster          CI_DESCRIPTION="Vert.x | 100: HTTP API"     CI_TYPE="launch.openshift.io .zip" LOSIOT_TARGET=PROD LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M100_HTTP_API     LOSIOT_RUNTIME=VERTX | oc apply -f -
+oc process losiot-template CI_NAME=losiot-prod-zip-vertx-health-checks-booster CI_DESCRIPTION="Vert.x | 104: Health Check" CI_TYPE="launch.openshift.io .zip" LOSIOT_TARGET=PROD LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M104_HEALTH_CHECK LOSIOT_RUNTIME=VERTX | oc apply -f -
+
+oc process losiot-template CI_NAME=losiot-prod-zip-spring-boot-http-booster         CI_DESCRIPTION="Spring Boot | 100: HTTP API"     CI_TYPE="launch.openshift.io .zip" LOSIOT_TARGET=PROD LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M100_HTTP_API     LOSIOT_RUNTIME=SPRING_BOOT | oc apply -f -
+oc process losiot-template CI_NAME=losiot-prod-zip-spring-boot-health-check-booster CI_DESCRIPTION="Spring Boot | 104: Health Check" CI_TYPE="launch.openshift.io .zip" LOSIOT_TARGET=PROD LOSIOT_DEPLOYMENT_TYPE=ZIP LOSIOT_MISSION=M104_HEALTH_CHECK LOSIOT_RUNTIME=SPRING_BOOT | oc apply -f -
+

280bd50ff71600da716c46d734695eedd384353c
Ladislav Thon <lthon@redhat.com>
2017-09-12 23:14:54 +0200

    add ability to configure a mirror of all external Maven repositories

--- ci-setup/README.md
+++ ci-setup/README.md
@@ -27,8 +27,13 @@ for F in *.yml ; do oc apply -f $F ; done
 
 Just running `./update-pipelines.sh` will generate a random GitHub secret
 for each pipeline. That's impractical for setting up webhooks for CI.
-Hence, it's possible to pass a parameter `./update-pipelines.sh XXX`
-which will then be used as a GitHub secret for all pipelines.
+So, when an environment variable `CI_GITHUB_SECRET` is set when running
+`./update-pipelines.sh`, its value will be used as a GitHub secret for all
+pipelines.
+
+```bash
+CI_GITHUB_SECRET=xxx ./update-pipelines.sh`
+```
 
 To get all the webhook URLs that have to be set in GitHub, run:
 
@@ -37,3 +42,16 @@ oc describe bc | grep webhooks
 ```
 
 The webhook is of type `application/json`.
+
+## Using a Maven mirror
+
+Sometimes, it is necessary to run tests with artifacts that aren't
+available in any public Maven repository. If an environment variable
+`MAVEN_MIRROR` is set when running `./update-pipelines.sh`, the
+`BuildConfig` templates are supposed to use its value as a URL
+of a Maven proxy that is configured in `~/.m2/settings.xml` as
+a mirror of `external:*`
+
+```bash
+MAVEN_MIRROR=http://... ./update-pipelines.sh
+```
--- ci-setup/update-pipelines.sh
+++ ci-setup/update-pipelines.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-SHARED_SECRET=""
+ADDITIONAL_OPTIONS=""
 
-if [[ $1 ]] ; then
-  SHARED_SECRET="CI_GITHUB_SECRET=$1"
+if [[ ! -z $CI_GITHUB_SECRET ]] ; then
+  ADDITIONAL_OPTIONS="$ADDITIONAL_OPTIONS CI_GITHUB_SECRET=$CI_GITHUB_SECRET"
+fi
+
+if [[ ! -z $MAVEN_MIRROR ]] ; then
+  ADDITIONAL_OPTIONS="$ADDITIONAL_OPTIONS MAVEN_MIRROR=$MAVEN_MIRROR"
 fi
 
 # CI boosters
 
-oc process ci-basic-template CI_NAME=ci-wfswarm-rest-http         CI_DESCRIPTION="WildFly Swarm | 100: HTTP API"        CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-rest-http $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-wfswarm-rest-http-crud    CI_DESCRIPTION="WildFly Swarm | 101: CRUD"            CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-rest-http-crud $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-wfswarm-configmap         CI_DESCRIPTION="WildFly Swarm | 102: ConfigMap"       CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-configmap $SHARED_SECRET | oc apply -f -
-oc process ci-sso-template   CI_NAME=ci-wfswarm-rest-http-secured CI_DESCRIPTION="WildFly Swarm | 103: SSO"             CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-rest-http-secured $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-wfswarm-health-check      CI_DESCRIPTION="WildFly Swarm | 104: Health Check"    CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-wfswarm-circuit-breaker   CI_DESCRIPTION="WildFly Swarm | 106: Circuit Breaker" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-circuit-breaker $SHARED_SECRET | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-wfswarm-rest-http         CI_DESCRIPTION="WildFly Swarm | 100: HTTP API"        CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-rest-http $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-wfswarm-rest-http-crud    CI_DESCRIPTION="WildFly Swarm | 101: CRUD"            CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-rest-http-crud $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-wfswarm-configmap         CI_DESCRIPTION="WildFly Swarm | 102: ConfigMap"       CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-configmap $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-sso-template   CI_NAME=ci-wfswarm-rest-http-secured CI_DESCRIPTION="WildFly Swarm | 103: SSO"             CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-rest-http-secured $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-wfswarm-health-check      CI_DESCRIPTION="WildFly Swarm | 104: Health Check"    CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-wfswarm-circuit-breaker   CI_DESCRIPTION="WildFly Swarm | 106: Circuit Breaker" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-circuit-breaker $ADDITIONAL_OPTIONS | oc apply -f -
 
-oc process ci-basic-template CI_NAME=ci-vertx-http-booster            CI_DESCRIPTION="Vert.x | 100: HTTP API"        CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-http-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-vertx-crud-booster            CI_DESCRIPTION="Vert.x | 101: CRUD"            CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-crud-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-vertx-configmap-booster       CI_DESCRIPTION="Vert.x | 102: ConfigMap"       CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-configmap-booster $SHARED_SECRET | oc apply -f -
-oc process ci-sso-template   CI_NAME=ci-vertx-secured-http-booster    CI_DESCRIPTION="Vert.x | 103: SSO"             CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-secured-http-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-vertx-health-checks-booster   CI_DESCRIPTION="Vert.x | 104: Health Check"    CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-health-checks-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-vertx-circuit-breaker-booster CI_DESCRIPTION="Vert.x | 106: Circuit Breaker" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-circuit-breaker-booster $SHARED_SECRET | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-vertx-http-booster            CI_DESCRIPTION="Vert.x | 100: HTTP API"        CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-http-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-vertx-crud-booster            CI_DESCRIPTION="Vert.x | 101: CRUD"            CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-crud-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-vertx-configmap-booster       CI_DESCRIPTION="Vert.x | 102: ConfigMap"       CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-configmap-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-sso-template   CI_NAME=ci-vertx-secured-http-booster    CI_DESCRIPTION="Vert.x | 103: SSO"             CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-secured-http-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-vertx-health-checks-booster   CI_DESCRIPTION="Vert.x | 104: Health Check"    CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-health-checks-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-vertx-circuit-breaker-booster CI_DESCRIPTION="Vert.x | 106: Circuit Breaker" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/openshiftio-vertx-boosters/vertx-circuit-breaker-booster $ADDITIONAL_OPTIONS | oc apply -f -
 
-oc process ci-basic-template CI_NAME=ci-spring-boot-http-booster           CI_DESCRIPTION="Spring Boot | 100: HTTP API"        CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-http-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-spring-boot-crud-booster           CI_DESCRIPTION="Spring Boot | 101: CRUD"            CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-crud-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-spring-boot-configmap-booster      CI_DESCRIPTION="Spring Boot | 102: ConfigMap"       CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-configmap-booster $SHARED_SECRET | oc apply -f -
-oc process ci-sso-template   CI_NAME=ci-spring-boot-http-secured-booster   CI_DESCRIPTION="Spring Boot | 103: SSO"             CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-http-secured-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-spring-boot-health-check-booster   CI_DESCRIPTION="Spring Boot | 104: Health Check"    CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-health-check-booster $SHARED_SECRET | oc apply -f -
-oc process ci-basic-template CI_NAME=ci-springboot-circuit-breaker-booster CI_DESCRIPTION="Spring Boot | 106: Circuit Breaker" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-circuit-breaker-booster $SHARED_SECRET | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-spring-boot-http-booster           CI_DESCRIPTION="Spring Boot | 100: HTTP API"        CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-http-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-spring-boot-crud-booster           CI_DESCRIPTION="Spring Boot | 101: CRUD"            CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-crud-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-spring-boot-configmap-booster      CI_DESCRIPTION="Spring Boot | 102: ConfigMap"       CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-configmap-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-sso-template   CI_NAME=ci-spring-boot-http-secured-booster   CI_DESCRIPTION="Spring Boot | 103: SSO"             CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-http-secured-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-spring-boot-health-check-booster   CI_DESCRIPTION="Spring Boot | 104: Health Check"    CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-health-check-booster $ADDITIONAL_OPTIONS | oc apply -f -
+oc process ci-basic-template CI_NAME=ci-springboot-circuit-breaker-booster CI_DESCRIPTION="Spring Boot | 106: Circuit Breaker" CI_TYPE="Booster repo" CI_GIT_URL=https://github.com/snowdrop/spring-boot-circuit-breaker-booster $ADDITIONAL_OPTIONS | oc apply -f -
 
 exit
 # TODO Losiot needs a lot of changes

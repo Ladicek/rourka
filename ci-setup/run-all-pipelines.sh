d9d54be04461cb4f8a38ae3772d8b1eabc79915b
Ladislav Thon <lthon@redhat.com>
2017-09-15 15:20:34 +0200

    the script to run all pipelines no longer downloads the results

--- ci-setup/run-all-pipelines.sh
+++ ci-setup/run-all-pipelines.sh
@@ -3,6 +3,3 @@
 for B in $(oc get bc -o name | grep 'ci-\|losiot-' | sed -e 's|buildconfig/||') ; do
   oc start-build $B --wait
 done
-
-wget -O junit.zip $(oc get route rourka -o jsonpath='http://{.spec.host}/junit')
-

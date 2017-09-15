97bca06002b4537421ace9006efa3f3c014d67b1
Ladislav Thon <lthon@redhat.com>
2017-09-15 15:27:56 +0200

    the script to run all pipelines now retries up to 5 times

--- ci-setup/run-all-pipelines.sh
+++ ci-setup/run-all-pipelines.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 for B in $(oc get bc -o name | grep 'ci-\|losiot-' | sed -e 's|buildconfig/||') ; do
-  oc start-build $B --wait
+  echo "---------- $B ----------"
+  for I in $(seq 1 5) ; do
+    echo "Attempt #$I..."
+    oc start-build $B --wait && break
+    sleep 5
+  done
 done

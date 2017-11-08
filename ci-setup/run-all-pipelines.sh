061d20a16179bde54977d39d19aadff288c7f0b9
Ladislav Thon <lthon@redhat.com>
2017-11-08 14:55:13 +0100

    don't retry WildFly Swarm ConfigMap booster, that one is stable

--- ci-setup/run-all-pipelines.sh
+++ ci-setup/run-all-pipelines.sh
@@ -7,7 +7,7 @@ for B in $(oc get bc -o name | grep 'ci-\|losiot-' | sed -r -e 's|buildconfigs?/
   GIT_SHA=$(git ls-remote $GIT_URL $GIT_REF | awk '{print $1}')
   echo "${GIT_URL}@${GIT_REF}: ${GIT_SHA}"
 
-  if [[ $B =~ configmap ]] ; then
+  if [[ $B =~ spring-boot-configmap ]] || [[ $B =~ vertx-configmap-booster ]] ; then
     for I in $(seq 1 5) ; do
       echo "Attempt #$I..."
       oc start-build $B --wait && break

674abdd19d44d9cbbeb05728b795ebfd47fdd1a7
Ladislav Thon <lthon@redhat.com>
2017-11-08 12:54:32 +0100

    improve run-all-pipelines.sh
    This commit improves the `run-all-pipelines.sh` script in 2 ways:
    
    1. Only the `configmap` boosters are retried, because these are known
       to sometimes fail intermittently. Failures of other boosters
       are known to be permanent and it doesn't make sense to retry.
    2. Before starting a build, Git information are printed: the repo URL,
       the selected ref and the corresponding commit hash.

--- ci-setup/run-all-pipelines.sh
+++ ci-setup/run-all-pipelines.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-for B in $(oc get bc -o name | grep 'ci-\|losiot-' | sed -e 's|buildconfig/||') ; do
+for B in $(oc get bc -o name | grep 'ci-\|losiot-' | sed -r -e 's|buildconfigs?/||') ; do
   echo "---------- $B ----------"
-  for I in $(seq 1 5) ; do
-    echo "Attempt #$I..."
-    oc start-build $B --wait && break
-    sleep 5
-  done
+  GIT_URL=$(oc get bc $B -o jsonpath='{.spec.source.git.uri}')
+  GIT_REF=$(oc get bc $B -o jsonpath='{.spec.source.git.ref}')
+  GIT_SHA=$(git ls-remote $GIT_URL $GIT_REF | awk '{print $1}')
+  echo "${GIT_URL}@${GIT_REF}: ${GIT_SHA}"
+
+  if [[ $B =~ configmap ]] ; then
+    for I in $(seq 1 5) ; do
+      echo "Attempt #$I..."
+      oc start-build $B --wait && break
+      sleep 5
+    done
+  else
+    oc start-build $B --wait
+  fi
 done

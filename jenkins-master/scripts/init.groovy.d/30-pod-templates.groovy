8e44cfd5cd662b0405863d13dac9a7a328fe7389
Ladislav Thon <lthon@redhat.com>
2018-01-11 19:11:00 +0100

    add a Jenkins slave suitable for running Jenkins Job Builder

--- jenkins-master/scripts/init.groovy.d/30-pod-templates.groovy
+++ jenkins-master/scripts/init.groovy.d/30-pod-templates.groovy
@@ -4,23 +4,28 @@ import org.csanchez.jenkins.plugins.kubernetes.ContainerTemplate
 import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud
 import org.csanchez.jenkins.plugins.kubernetes.PodTemplate
 
-def kube = Jenkins.instance.clouds.getByName("openshift") as KubernetesCloud
+def podTemplate = { name, image ->
+    return new PodTemplate().with {
+        it.name = name
+        it.label = name
+        it.nodeUsageMode = Node.Mode.NORMAL
+        it.serviceAccount = 'jenkins'
+        it.containers = [
+                new ContainerTemplate('jnlp', image).with {
+                    it.alwaysPullImage = true
+                    it.workingDir = '/tmp'
+                    it.command = ''
+                    it.args = '${computer.jnlpmac} ${computer.name}'
+                    return it
+                }
+        ]
+        return it
+    }
+}
+
+def kube = Jenkins.instance.clouds.getByName('openshift') as KubernetesCloud
 
 kube.templates = [
-        new PodTemplate().with {
-            it.name = "rourka-maven"
-            it.label = "rourka-maven"
-            it.nodeUsageMode = Node.Mode.NORMAL
-            it.serviceAccount = "jenkins"
-            it.containers = [
-                    new ContainerTemplate("jnlp", "ladicek/rourka-jenkins-slave-maven").with {
-                        it.alwaysPullImage = true
-                        it.workingDir = "/tmp"
-                        it.command = ""
-                        it.args = '${computer.jnlpmac} ${computer.name}'
-                        return it
-                    }
-            ]
-            return it
-        }
+        podTemplate('rourka-maven', 'ladicek/rourka-jenkins-slave-maven'),
+        podTemplate('rourka-jjb', 'ladicek/rourka-jenkins-slave-jjb'),
 ]
---
+++ jenkins-slave-jjb/Dockerfile
@@ -0,0 +1,15 @@
+FROM openshift/jenkins-slave-base-centos7:latest
+MAINTAINER Ladislav Thon <lthon@redhat.com>
+
+USER root
+
+RUN yum -y install epel-release && \
+    yum -y install python-pip && \
+    yum clean all -y && \
+    pip install --upgrade pip six 'jenkins-job-builder>=2.0.0.0b3' && \
+    sed -i "s/return b'Basic ' + base64.b64encode(auth)/return b'Bearer ' + password/" /usr/lib/python2.7/site-packages/jenkins/__init__.py
+
+COPY jenkins_jobs.ini /etc/jenkins_jobs/
+COPY jjb /usr/local/bin/
+
+USER 1001
